6da65831e2480f83fdec35573095b90de19dcef2
twój login do GitHuba <96842769+MaksBernatowicz@users.noreply.github.com>
2022-04-10 14:17:00 +0200

    added onChange events for inputs

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ const App = () => {
       <div className="app-background">
         <div className="main-container">
           <div className="add-item-box">
-            <input value={itemName} className="add-item-input" placeholder="Add item name.."/>
-            <input value={itemInfo} className="add-item-input" placeholder="Add item name.."/>
-            <input value={itemPrice === null ? "" : itemPrice} className="add-item-input" placeholder="Add item name.."/>
-            <select value={itemCategory} className="add-item-input">
+            <input value={itemName} onChange={(event) => setItemName(event.target.value)} className="add-item-input" placeholder="Add item name.."/>
+            <input value={itemInfo} onChange={(event) => setItemInfo(event.target.value)} className="add-item-input" placeholder="Add item name.."/>
+            <input value={itemPrice === null ? "" : itemPrice} onChange={(event) => setItemPrice(parseFloat(event.target.value))} className="add-item-input" placeholder="Add item name.."/>
+            <select value={itemCategory} onChange={(event) => setItemCategory(event.target.value)} className="add-item-input">
 
             </select>
           </div>
