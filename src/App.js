7fd4910f84f73af35665365233bff7b43fae0671
twój login do GitHuba <96842769+MaksBernatowicz@users.noreply.github.com>
2022-04-10 14:10:39 +0200

    added useState() for inputs values

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 const App = () => {
+
+
+    const [itemName, setItemName] = useState("");
+    const [itemInfo, setItemInfo] = useState("");
+    const [itemPrice, setItemPrice] = useState(null);
+    const [itemCategory, setItemCategory] = useState("choose category..");
 
   return (
       <div className="app-background">
         <div className="main-container">
           <div className="add-item-box">
-            <input className="add-item-input" placeholder="Add item name.."/>
-            <input className="add-item-input" placeholder="Add item name.."/>
-            <input className="add-item-input" placeholder="Add item name.."/>
-            <select className="add-item-input">
+            <input value={itemName} className="add-item-input" placeholder="Add item name.."/>
+            <input value={itemInfo} className="add-item-input" placeholder="Add item name.."/>
+            <input value={itemPrice === null ? "" : itemPrice} className="add-item-input" placeholder="Add item name.."/>
+            <select value={itemCategory} className="add-item-input">
 
             </select>
           </div>
